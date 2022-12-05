c688ec6bb7630b5d4de7a54e6ff356ddd46df868
saimang12 <114594540+saimang12@users.noreply.github.com>
2022-12-05 22:05:47 +0900

    Add files via upload

--- js/homework03.js
+++ js/homework03.js
@@ -1,3 +1,13 @@
 $('.mainSlide').slick({
-    // arrows:false,
-})
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+});
+
+$('.play').click(function () {
+    $('.mainSlide').slick('slickPlay');
+});
+
+$('.stop').click(function () {
+    $('.mainSlide').slick('slickPause');
+});
