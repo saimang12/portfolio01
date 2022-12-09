946d823c4efb924dc3008476cb971daa88ba02d4
saimang12 <114594540+saimang12@users.noreply.github.com>
2022-12-09 12:30:25 +0900

    Add files via upload

--- js/homework03.js
+++ js/homework03.js
@@ -2,6 +2,8 @@ $('.mainSlide').slick({
     dots: true,
     autoplay: true,
     autoplaySpeed: 3000,
+    pauseOnHover: false,
+
 });
 
 $('.play').click(function () {
@@ -11,3 +13,76 @@ $('.play').click(function () {
 $('.stop').click(function () {
     $('.mainSlide').slick('slickPause');
 });
+
+$('.icon').slick({
+
+});
+
+$('.mainContents .tap01').on('click', function () {
+    $(this).addClass('on').siblings().removeClass('on');
+    $('.item01').addClass('on').siblings().removeClass('on')
+});
+
+$('.mainContents .tap02').on('click', function () {
+    $(this).addClass('on').siblings().removeClass('on');
+    $('.item02').addClass('on').siblings().removeClass('on')
+});
+
+$('.mainContents .tap03').on('click', function () {
+    $(this).addClass('on').siblings().removeClass('on');
+    $('.item03').addClass('on').siblings().removeClass('on')
+});
+
+$('.mainContents .tap04').on('click', function () {
+    $(this).addClass('on').siblings().removeClass('on');
+    $('.item04').addClass('on').siblings().removeClass('on')
+});
+
+$('.control01').on('click', function () {
+    $(this).toggleClass('on');
+    $(this).hasClass('on') ?
+        $('.video01').trigger('pause')
+        : $('.video01').trigger('play');
+});
+
+$('.control02').on('click', function () {
+    $(this).toggleClass('on');
+    $(this).hasClass('on') ?
+        $('.video02').trigger('pause')
+        : $('.video02').trigger('play');
+});
+
+$('.control03').on('click', function () {
+    $(this).toggleClass('on');
+    $(this).hasClass('on') ?
+        $('.video03').trigger('pause')
+        : $('.video03').trigger('play');
+});
+
+
+// $('.family button').on('click', function () {
+//     $(this).addClass('on');
+//     $('.site_list').addClass('on');
+
+
+// });
+
+
+
+$('.family button').click(function () {
+    if ($('.site_list').hasClass("on")) {
+        $('.site_list').removeClass("on");
+    } else {
+        $('.site_list').addClass("on");
+    }
+});
+
+$('.model').click(function () {
+    if ($('.wrap').hasClass("on")) {
+        $('.wrap').removeClass("on");
+    } else {
+        $('.wrap').addClass("on");
+    }
+});
+
+
