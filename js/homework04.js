23fef4ddf6d81c2f43b366fedf303cd5779f4599
saimang12 <114594540+saimang12@users.noreply.github.com>
2022-12-14 17:45:15 +0900

    Add files via upload

--- js/homework04.js
+++ js/homework04.js
@@ -1,3 +1,27 @@
-$('.arrow').on('click', function () {
-    $('.mainContents').offset({ top: 50, left: 50 });
-})
+$(function () {
+    $('.arrow').click(function () {
+        $('html, body').animate({ scrollTop: $(this.hash).offset.top }, 3000);
+    });
+});
+
+$(window).on('scroll', function () {
+    console.log('스크롤 : ', $(window).scrollTop());
+    // 스크롤된 양을 구해서 0보다 크면 클래스를 붙이고 아니면 클래스를 땐다.
+    // if(스크롤양 > 0) {클래스를 붙이} else {클래스를 땐}
+    // 스클롤량을 구하기 : $(window).scrollTop()
+
+    if ($(window).scrollTop() > 0) {
+        $('.header').addClass('on');
+    } else {
+        $('.header').removeClass('on');
+    }
+});
+
+$('.family_site').click(function () {
+    if ($('.family_link').hasClass("on")) {
+        $('.family_link').removeClass("on");
+    } else {
+        $('.family_link').addClass("on");
+    }
+});
+
